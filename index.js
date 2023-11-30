23ef7ea4005c787298b891d949a0064e2a7bea7f
awellsbiz <awellsbiz@gmail.com>
2023-11-29 21:47:40 -0800

    routes set up

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-console.log("yerrrrr")
+const express = require('express')
+const app = express()
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1> It is all beginning </h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.send(persons)
+})
+
+app.get('/api/persons/:id', (req,res) => {
+    const id = Number(req.params.id)
+    const person = persons.find( person => person.id === id)
+    if (person) {
+        res.send(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.get('/info', (req,res) => {
+    const currentDate = new Date()
+    const numberOfPeople = persons.length
+    res.send(`<h1>The info page </h1> <p> You have ${numberOfPeople} people in the phone book </br> ${currentDate} </p>`)
+})
+
+app.delete('/api/persons/:id', (req,res) =>{
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
+})
+const PORT = 3000
+app.listen(PORT)
